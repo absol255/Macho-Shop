4b1f997b7fd0ba523ecf900a4a9f6d5c6cdfb400
absol255 <mewoisthebest123456789@gmail.com>
2026-05-30 06:35:24 -0700

    eraser with wrong values

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,9 @@
     const machoBadgeBuy = document.getElementById("macho-badge-buy");
     const machoBadgeAmount = document.getElementById("macho-badge-amount");
     const machoBadgeBank = document.getElementById("macho-badge-bank");
+    const machoEraserBuy = document.getElementById("macho-eraser-buy");
+    const machoEraserAmount = document.getElementById("macho-eraser-amount");
+    const machoEraserBank = document.getElementById("macho-eraser-bank");
 
     machoFlagBuy.addEventListener('click', function () {
         const amount = parseInt(machoFlagAmount.value);
@@ -42,6 +45,25 @@
                     return;
                 }
                 alert("Bought a macho badge!")
+    })});
+
+    machoEraserBuy.addEventListener('click', function () {
+        const amount = parseInt(machoEraserAmount.value);
+        const bank = parseInt(machoEraserBank.value);
+
+        fetch("/api/buy", {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({bank: bank, amount: amount, item: "eraser", item_cost: 0 || 0 }),
+        })
+            .then(async function (r) { return r.json().then(function (d) { return { ok: r.ok, d: d }; }); })
+            .then(function (res) {
+                if (!res.ok) {
+                    alert(res.d.error || "Could not buy a macho pink eraser");
+                    return;
+                }
+                alert("Bought a macho pink eraser!")
     })});
 
 })();
